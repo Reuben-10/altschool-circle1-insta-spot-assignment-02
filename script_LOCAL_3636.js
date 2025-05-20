068b5eb107174ed9aa8f6c16f904e3c10da5a5ec
Busari Olanrewaju <128599951+Horlanrewajucode@users.noreply.github.com>
2025-05-20 03:26:00 +0100

    Olanrewaju: implemented the profile edit button functionality using a modal and improved the styling for it (#30)
    * Added fuctionality for the profile
    
    * added functionality for the profile
    
    * added fuctionality for profile and update styling
    
    ---------
    
    Co-authored-by: Reuben Agbor <reubenagbor10@gmail.com>

--- script_LOCAL_3636.js
+++ script_LOCAL_3636.js
@@ -1,23 +1,3 @@
-// Style for .liked class
-const style = document.createElement("style");
-style.textContent = `
-  .like-icon.liked path {
-    fill: red;
-    stroke: red;
-    // transform: scale(1.2); Having this here will cause the icon to scale up when clicked
-    // transition: transform 3s ease; 
-  }
-`;
-document.head.appendChild(style);
-
-// Adding event listeners to like icons
-const likeIcons = document.querySelectorAll(".like-icon");
-likeIcons.forEach((icon) => {
-  icon.addEventListener("click", () => {
-    icon.classList.toggle("liked");
-  });
-});
-
 // Store original values
 let originalName = "Bessie Coleman";
 let originalTitle = "Civil Aviator";
