1f6f48240bdf5944aae29e7fe40c0572aacccc2e
Horlanrewajucode <buzareeolanrewaju01@gmail.com>
2025-05-22 16:01:34 +0100

    improve modal functionality and styling

--- js/modules/profileEditor.js
+++ js/modules/profileEditor.js
@@ -1,14 +1,13 @@
 // Store original values
-let originalName = "Bessie Coleman";
-let originalTitle = "Civil Aviator";
-let originalBio = "LoremipsumdolorsitametconsecteturadipisicinAsperiores itaqueimpedittemporequiporro!Lorem ipsumdolsitametconsecteturadipisicingelitQuasimolestias";
+let originalName = "";
+let originalTitle = "";
 
 // Function to open the modal
 function openEditModal() {
   // Populate modal inputs with original values
   document.getElementById("editName").value = originalName;
   document.getElementById("editTitle").value = originalTitle;
-  document.getElementById("editBio").value = originalBio;
+  // document.getElementById("editBio").value = originalBio;
 
   document.getElementById("editModal").style.display = "block";
   //  document.getElementById("editModal").showModal();
@@ -23,12 +22,20 @@ function closeEditModal() {
 function saveProfile() {
   const newName = document.getElementById("editName").value.trim();
   const newTitle = document.getElementById("editTitle").value.trim();
-  const newBio = document.getElementById("editBio").value.trim();
+  // const newBio = document.getElementById("editBio").value.trim();
 
-  if (!newName || !newTitle || !newBio) {
+  if (!newName || !newTitle) {
     document.getElementById("profileName").textContent = originalName;
     document.getElementById("profileTitle").textContent = originalTitle;
-    document.getElementById("profileBio").textContent = originalBio;
+    // document.getElementById("profileBio").textContent = originalBio;
+
+    // const saveBtn = document.getElementById('saveBtn')
+
+    // if (newName && newTitle) {
+    //   saveBtn.disabled = false
+    // } else {
+    //   saveBtn.disabled = true
+    // }
 
     closeEditModal();
     return;
@@ -36,7 +43,7 @@ function saveProfile() {
 
   document.getElementById("profileName").textContent = newName;
   document.getElementById("profileTitle").textContent = newTitle;
-  document.getElementById("profileBio").textContent = newBio;
+  // document.getElementById("profileBio").textContent = newBio;
 
   const fileInput = document.getElementById("editImage");
 
