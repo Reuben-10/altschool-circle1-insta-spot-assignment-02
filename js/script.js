d245f818a15bde39b9d0cacd6a4d03a59ed39c94
Ruqayah1204 <mhiedey98@gmail.com>
2025-05-22 18:46:41 +0100

    Rukayat: Added autofocus on first card

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
 import { displayCards } from "./modules/displayCards.js";
+import { setAutoFocus} from "./modules/focusCard.js";
 import { openEditModal, closeEditModal, saveProfile } from "./modules/profileEditor.js";
 
 // Initialize
 displayCards(".container");
+setAutoFocus(".container");
+// enableHoverFocus(".container");
 
 // Attach event listeners (ensure elements exist in HTML)
 document.getElementById("editBtn").addEventListener("click", openEditModal);
