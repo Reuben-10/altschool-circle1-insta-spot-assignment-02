181b0da88e55c4d6ee88e321949d6c4039369216
Horlanrewajucode <buzareeolanrewaju01@gmail.com>
2025-05-22 16:06:40 +0100

    Removed all unneccessary comments

--- js/modules/profileEditor.js
+++ js/modules/profileEditor.js
@@ -7,10 +7,8 @@ function openEditModal() {
   // Populate modal inputs with original values
   document.getElementById("editName").value = originalName;
   document.getElementById("editTitle").value = originalTitle;
-  // document.getElementById("editBio").value = originalBio;
 
   document.getElementById("editModal").style.display = "block";
-  //  document.getElementById("editModal").showModal();
 }
 
 // Function to hide the modal
@@ -22,28 +20,17 @@ function closeEditModal() {
 function saveProfile() {
   const newName = document.getElementById("editName").value.trim();
   const newTitle = document.getElementById("editTitle").value.trim();
-  // const newBio = document.getElementById("editBio").value.trim();
 
   if (!newName || !newTitle) {
     document.getElementById("profileName").textContent = originalName;
     document.getElementById("profileTitle").textContent = originalTitle;
-    // document.getElementById("profileBio").textContent = originalBio;
-
-    // const saveBtn = document.getElementById('saveBtn')
-
-    // if (newName && newTitle) {
-    //   saveBtn.disabled = false
-    // } else {
-    //   saveBtn.disabled = true
-    // }
-
     closeEditModal();
+
     return;
   }
 
   document.getElementById("profileName").textContent = newName;
   document.getElementById("profileTitle").textContent = newTitle;
-  // document.getElementById("profileBio").textContent = newBio;
 
   const fileInput = document.getElementById("editImage");
 
